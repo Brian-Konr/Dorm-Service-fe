d015f061bb595001bcee36bf05552a3b34b2dd05
Brian-Konr <b08705038@ntu.edu.tw>
2021-11-03 01:33:51 +0800

    still working on login and direct to home page with react-router-dom

--- frontend/src/containers/login_page.js
+++ frontend/src/containers/login_page.js
@@ -1,69 +1,77 @@
 import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
 
 const Login_Page = ({setLogin, setName}) => {
 
-    const onFinish = (values) => {
-        console.log('Received values of form: ', values);
-    };
+    const[userName, setUserName] = useState("");
+    const[password, setPassword] = useState("");
 
-    var tempValue = "";
-    const onChange = (e) => {
-        tempValue = e.target.value;
-    };
+    // const onChange = (e) => {
+    //     setUserName(e.target.value);
+    // };
 
     const handleLogin = () => {
-        setName(tempValue);
-        setLogin(true);
+        if(userName === "aaa" && password === "bbb") {
+            setName(userName);
+            setLogin(true);
+            // redirect to "/"
+            console.log("ok");
+        }
+        else {
+            setLogin(false);
+            console.log("not ok");
+        }
     }
 
   return (
     <>
-    <h1 className="login_title">Login</h1>
-    <div className="login_page">
-        <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{remember: true,}}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="username"
-                rules={[
-                {
-                    required: true,
-                    message: 'Please input your Username!',
-                },
-                ]}
+        <h1 className="login_title">Login</h1>
+        <div className="login_page">
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{remember: true,}}
             >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" onChange={onChange}/>
-            </Form.Item>
+                <Form.Item
+                    name="username"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!',
+                    },
+                    ]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />} value = {userName} placeholder="Username" onChange={(e) => setUserName(e.target.value)}/>
+                </Form.Item>
 
-            <Form.Item
-                name="password"
-                rules={[
-                {
-                    required: true,
-                    message: 'Please input your Password!',
-                },
-                ]}
-            >
-                <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-                />
-            </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="Password"
+                        value = {password}
+                        onChange = {(e) => setPassword(e.target.value)}
+                    />
+                </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="wide-form-button" onClick={handleLogin}>
-                    <Link to="/">Login</Link>
-                </Button>
-                Don't have an Account?  <a href="/signUp">Sign Up</a>
-            </Form.Item>
-        </Form>
-    </div>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="wide-form-button">
+                        <Link to="/">Login</Link>
+                    </Button>
+                    Don't have an Account?  <a href="/signUp">Sign Up</a>
+                </Form.Item>
+            </Form>
+        </div>
     </>
   );
 

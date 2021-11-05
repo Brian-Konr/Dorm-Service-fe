f8567290cd711bb050473cd9a87d99b7a0036fe0
peifish1124 <b08705026@ntu.edu.tw>
2021-11-05 23:41:19 +0800

    [Chen] personal page and add post page

--- frontend/src/containers/personal_page.js
+++ frontend/src/containers/personal_page.js
@@ -9,12 +9,12 @@ const Personal_Page = ({login,name,setCurrent,current, userId}) => {
     const medal_component = [<Icon icon="whh:medal" color="#c9c9c9" height="50" />,<Icon icon="fa-solid:medal" color="#c9c9c9" height="50" />,<Icon icon="whh:medalbronze" color="#d3976e" height="50" />,<Icon icon="whh:medalsilver" color="#b2c1c0" height="50" />,<Icon icon="whh:medalgold" color="#e9a012" height="50" />];
     const medal_name = ['實習生','新星','達人','專家','大師']
     
-    const [level, setLevel] = useState([]);
     const [gender, setGender] = useState("");
     const [phoneNumber, setPhoneNum] = useState("");
     const [fbUrl, setFbUrl] = useState("");
     const [start, setStart] = useState(true);
-    const [temp, setTemp] = useState();
+    const [level_1, setLevel_1] = useState();const [level_2, setLevel_2] = useState();
+    const [level_3, setLevel_3] = useState();const [level_4, setLevel_4] = useState();
     
     // 以下是需要跟後端接的資料
     async function personalDetail(){
@@ -22,16 +22,13 @@ const Personal_Page = ({login,name,setCurrent,current, userId}) => {
             // GET api
             let res = await axios.get(`http://127.0.0.1:8000/users/${userId}`, {});
                 if(res.status === 200) { 
-                    // console.log(res.data); 
                     setGender(res.data.gender);
                     setPhoneNum(res.data.phoneNum);
                     setFbUrl(res.data.fbUrl);
-                    res.data.userPoints.map( e => {
-                        let data = e.service_id;
-                        setLevel([...level,data]);
-                        console.log("hello");
-                        console.log(data);
-                    })
+                    setLevel_1(res.data.userPoints[0].level_id);
+                    setLevel_2(res.data.userPoints[1].level_id);
+                    setLevel_3(res.data.userPoints[2].level_id);
+                    setLevel_4(res.data.userPoints[3].level_id);
                 }
             return;
         } catch (error) {
@@ -62,31 +59,28 @@ const Personal_Page = ({login,name,setCurrent,current, userId}) => {
             <div className="personal_Name">{name}</div>
             <Divider orientation="left" plain>勳章牆</Divider>
             <div className="medal_part">
-                {
-                    // console.log(temp)
-                }
                 <div className="medal">
-                    {medal_component[level[0]-1]}
-                    <div>打蟑螂{medal_name[level[0]-1]}</div>
+                    {medal_component[level_1-1]}
+                    <div>載人{medal_name[level_1-1]}</div>
                 </div>
                 <div className="medal">
-                    {medal_component[level[1]-1]}
-                    <div>物品搬運{medal_name[level[1]-1]}</div>
+                    {medal_component[level_2-1]}
+                    <div>物品搬運{medal_name[level_2-1]}</div>
                 </div>
                 <div className="medal">
-                    {medal_component[level[2]-1]}
-                    <div>載人{medal_name[level[2]-1]}</div>
+                    {medal_component[level_3-1]}
+                    <div>打蟑螂{medal_name[level_3-1]}</div>
                 </div>
                 <div className="medal">
-                    {medal_component[level[3]-1]}
-                    <div>辦活動{medal_name[level[3]-1]}</div>
+                    {medal_component[level_4-1]}
+                    <div>辦活動{medal_name[level_4-1]}</div>
                 </div>
             </div>
 
             <Divider orientation="left" plain>基本資料</Divider>
             <div>
                 {item("用戶姓名",[(<p>{name}</p>)])}
-                {item("用戶性別",[(<p>{gender}</p>)])}
+                {item("用戶性別",[(<p>{gender==='O'?'Other':gender==='F'?'Female':gender==='M'?'Male':null}</p>)])}
                 {item("手機號碼",[(<p>{phoneNumber}</p>)])}
                 {item("臉書網址",[(<p>{fbUrl}</p>)])}
             </div>

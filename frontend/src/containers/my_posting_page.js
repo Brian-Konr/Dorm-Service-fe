21034154d3b49f99c030ea145652ffddef86bc45
guanyiiiii-0225 <zoememy@gmail.com>
2021-11-05 21:50:04 +0800

    ongoing post requested by login user

--- frontend/src/containers/my_posting_page.js
+++ frontend/src/containers/my_posting_page.js
@@ -2,7 +2,7 @@ import Navigation from '../containers/navigation';
 import Post_Table from '../components/post_table';
 import { useState } from 'react'
  
-const My_Posting_Page = ({login,name,setCurrent,current, setViewSelf}) => {
+const My_Posting_Page = ({login,name,setCurrent,current, setViewSelf, userId}) => {
   setViewSelf(true);
   // console.log("my posting page");
   const [serviceStatus, setserviceStatus] = useState("all");
@@ -14,7 +14,7 @@ const My_Posting_Page = ({login,name,setCurrent,current, setViewSelf}) => {
           </header> 
           <h1 className="page_title">檢視發起中任務</h1>
           {/* <div className="post_subTitle">以下為您刊登中的任務列表：</div> */}
-          <div className="post_table"><Post_Table Page = {"myPost"}  serviceStatus = {serviceStatus}/></div>
+          <div className="post_table"><Post_Table Page = {"myPost"}  serviceStatus = {serviceStatus} userId = {userId}/></div>
           {/* <div className="post_table"><Post_Table isMainPage = {"myPost"} serviceStatus = {serviceStatus}/></div> */}
         </div>
       );  
